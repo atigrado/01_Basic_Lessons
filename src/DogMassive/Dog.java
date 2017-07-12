19ae638bd38f242f953ae8bd3ecfa243a47308ee
atigrado <ak250483@gmail.com>
2017-07-12 23:00:09 +0300

    Пример очередности вызова методов

---
+++ src/DogMassive/Dog.java
@@ -0,0 +1,37 @@
+package DogMassive;
+
+/**
+ * Created by Artem Konyukhov on 29.06.2016.
+ */
+public class Dog {
+    String name;
+
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        dog1.bark();
+        dog1.name = "Барт";
+
+        Dog[] myDogs = new Dog[3];
+
+        myDogs[0] = new Dog();
+        myDogs[1] = new Dog();
+        myDogs[2] = dog1;
+
+        myDogs[0].name = "Фред";
+        myDogs[1].name = "Джордж";
+
+        System.out.print("Имя последней собаки -");
+        System.out.println(myDogs[2].name);
+
+        int x = 0;
+        while (x < myDogs.length) {
+            myDogs[x].bark();
+            x = x + 1;
+        }
+    }
+    public  void bark() {
+        System.out.println(name + " сказал Гав!");
+    }
+    public void eat() {}
+    public void chaseCat() {}
+}
